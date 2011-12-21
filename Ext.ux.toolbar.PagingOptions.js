2b36a46f139e352accad5c7f93bed5df9437185e
Arthur Kay <arthurakay@gmail.com>
2011-12-21 12:18:54 -0600

    Updated code whitespace

--- Ext.ux.toolbar.PagingOptions.js
+++ Ext.ux.toolbar.PagingOptions.js
@@ -7,50 +7,52 @@
  * @param {object} configObj
  */
 Ext.define('Ext.ux.toolbar.PagingOptions', {
-    extend: 'Ext.toolbar.Paging',
+    extend : 'Ext.toolbar.Paging',
 
-    getPagingItems: function() {
+    getPagingItems : function() {
         var me = this,
             pagingButtons = me.callParent();
 
         if (!Ext.ModelManager.getModel('PageSize')) {
             Ext.define('PageSize', {
-                extend: 'Ext.data.Model',
-                fields: [{ name: 'pagesize' , type: 'int'}]
+                extend : 'Ext.data.Model',
+                fields : [{ name : 'pagesize' , type : 'int'}]
             });
         }
 
         if (!me.pageSizeOptions) {
             me.pageSizeOptions = [
-                { pagesize: 10 },
-                { pagesize: 25 },
-                { pagesize: 50 },
-                { pagesize: 100 },
-                { pagesize: 250 },
-                { pagesize: 500 }
+                { pagesize : 10 },
+                { pagesize : 25 },
+                { pagesize : 50 },
+                { pagesize : 100 },
+                { pagesize : 250 },
+                { pagesize : 500 }
             ];
         }
 
         pagingButtons.push({
-            xtype: 'combobox',
-            queryMode: 'local',
-            triggerAction: 'all',
-            displayField: 'pagesize',
-            valueField: 'pagesize',
-            width: 100,
-            lazyRender: true,
-            enableKeyEvents: true,
-            value: me.pageSize,
-            forceSelection: me.forceSelection || false,
-            store: new Ext.data.Store({
-                model: 'PageSize',
-                data: me.pageSizeOptions
+            xtype           : 'combobox',
+            queryMode       : 'local',
+            triggerAction   : 'all',
+            displayField    : 'pagesize',
+            valueField      : 'pagesize',
+            width           : 100,
+            lazyRender      : true,
+            enableKeyEvents : true,
+            value           : me.pageSize,
+            forceSelection  : me.forceSelection || false,
+
+            store : new Ext.data.Store({
+                model : 'PageSize',
+                data  : me.pageSizeOptions
             }),
-            listeners: {
-                select: function(thisField, value) {
+
+            listeners : {
+                select : function(thisField, value) {
                     me.fireEvent('pagesizeselect', value[0].get('pagesize'));
                 },
-                keypress: function(thisField, eventObj) {
+                keypress : function(thisField, eventObj) {
                     if (eventObj.getKey() !== eventObj.ENTER) { return false; }
                     me.fireEvent('pagesizeselect', thisField.getValue());
                 }
